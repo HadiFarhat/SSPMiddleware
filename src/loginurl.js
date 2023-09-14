71f3131714a79cf99dc54aa06dc28ae06e2ca3d0
HadiFarhat <hadi.farhat@bigcommerce.com>
2023-09-14 14:36:37 +0100

    guest checkout

--- src/loginurl.js
+++ src/loginurl.js
@@ -1,43 +1,56 @@
-import jwt from 'jsonwebtoken';
-import { v4 as uuidv4 } from 'uuid';
-import request from 'request';
+import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from "uuid";
+import request from "request";
 
 // Get BC store time, then use it to ask for one-time passwordless autologin
 
 function gettime() {
-    return new Promise((resolve, reject) => {
-        var options = {
-            'method': 'GET',
-            'url': 'https://api.bigcommerce.com/stores/'+process.env.STORE_Hash+'/v2/time',
-            'headers': {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'X-Auth-Client': process.env.ClientId, // BC API client ID
-                'X-Auth-Token': process.env.Token // BC API token
-            }
-        };
-        request(options, function (error, response) {
-            if (error) reject(error);
-            const time = JSON.parse(response.body)["time"];
-            //console.log(time);
-            resolve(time);
-        });
+  return new Promise((resolve, reject) => {
+    var options = {
+      method: "GET",
+      url:
+        "https://api.bigcommerce.com/stores/" +
+        process.env.STORE_Hash +
+        "/v2/time",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Auth-Client": process.env.ClientId, // BC API client ID
+        "X-Auth-Token": process.env.Token, // BC API token
+      },
+    };
+    request(options, function (error, response) {
+      if (error) reject(error);
+      const time = JSON.parse(response.body)["time"];
+      //console.log(time);
+      resolve(time);
     });
+  });
 }
 
-
-export default async function getLoginUrl(customerId, storeHash, storeUrl, clientId, clientSecret, checkoutUrl) {
-    var dateCreated = await gettime();
-    const payload = {
-        "iss": clientId,
-        "iat": dateCreated,
-        "jti": uuidv4(),
-        "operation": "customer_login",
-        "store_hash": storeHash,
-        "customer_id": customerId,
-        "redirect_to": checkoutUrl
-    }
-    //console.log(payload);
-    let token = jwt.sign(payload, clientSecret, {algorithm:'HS256'});
+export default async function getLoginUrl(
+  customerId,
+  storeHash,
+  storeUrl,
+  clientId,
+  clientSecret,
+  checkoutUrl
+) {
+  var dateCreated = await gettime();
+  const payload = {
+    iss: clientId,
+    iat: dateCreated,
+    jti: uuidv4(),
+    operation: "customer_login",
+    store_hash: storeHash,
+    customer_id: customerId,
+    redirect_to: checkoutUrl,
+  };
+  //console.log(payload);
+  let token = jwt.sign(payload, clientSecret, { algorithm: "HS256" });
+  if (customerId === 0) {
+    return checkoutUrl
+  } else {
     return `${storeUrl}/login/token/${token}`;
-};
+  }
+}
